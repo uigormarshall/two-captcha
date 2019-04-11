6c1e81f6e44aa1a251474bc97e73d189bfb8b3aa
uigor <uigor.silva@acad.ifma.edu.br>
2019-04-11 12:17:59 +0000

    Update getId params

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,11 @@ const TwoCaptcha = require("../lib/index")
 require("dotenv").config()
 
 const Captcha = new TwoCaptcha(process.env.apiKey)
-
+const method = process.env.method
+const googleKey = process.env.googleKey
+const pageUrl = process.env.pageUrl
 //  improve the test
 test("Get Id", async t => {
-  t.assert(Captcha.getId(), "Attempt made successfully!")
+  t.assert(Captcha.getId(method, googleKey, pageUrl), "Attempt made successfully!")
   t.end()
 })
