05de82a2cf11d8dbdcee104579fe7d4b39267f07
uigor <uigor.silva@acad.ifma.edu.br>
2019-04-12 16:09:11 +0000

    Add method solve

--- test/index.test.js
+++ test/index.test.js
@@ -6,8 +6,21 @@ const Captcha = new TwoCaptcha(process.env.apiKey)
 const method = process.env.method
 const googleKey = process.env.googleKey
 const pageUrl = process.env.pageUrl
+let id = 00000
 //  improve the test
-test("Get Id", async t => {
-  t.assert(Captcha.getId(method, googleKey, pageUrl), "Attempt made successfully!")
+test("Get id", async t => {
+  resp = await  Captcha.getId(method, googleKey, pageUrl)
+  resp = JSON.parse(resp)
+  if(resp.status === 1){
+   id = resp.request
+  }
+  t.assert(resp, resp.request)
   t.end()
 })
+
+test("Solve", async t => {
+  let solve = await Captcha.solve(id)
+  t.assert(solve, solve.request)
+  t.end()
+})
+
